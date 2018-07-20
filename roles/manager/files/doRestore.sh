a3a8db046dc7a41855a219c6017babe0766fa93d
Mitsuru Nakakawaji <mitsuru@procube.jp>
2018-07-21 08:50:21 +0900

    many changes

--- roles/manager/files/doRestore.sh
+++ roles/manager/files/doRestore.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 LOGGER=${0##*/}@{{ inventory_hostname }}
-BACKUPDIR=$1
+BACKUPDIR=${1:-$HOME/backup}
 
 function message () {
   echo "INFO: $LOGGER:" $*
 }
 
-cd $BACKUPDIR
-
 shopt -s expand_aliases
 source ~/.bashrc
 dlg-{{ inventory_hostname }}
+cd $BACKUPDIR
 
 {%  for backup_script in backup_scripts %}
 # for restore {{ backup_script.name }}
 backup_file="backup-{{ backup_script.name }}-latest.{{ backup_script.ext | default('tar.gz') }}"
 if [ -r $backup_file ]; then
-{% if backup_script.restore_command is defined %}
   message "START restore for {{ backup_script.name }} from $backup_file"
+{% if backup_script.restore_command is defined %}
   {% if backup_script.restore_file is defined %}
-  docker cp $backup_file {{ backup_script.container }}:{{ backup_script.restore_file }} $backup_file
-  if docker exec {{ backup_script.container }} {{ backup_script.restore_command }}  ; then
+  docker cp -L $backup_file {{ backup_script.container }}:{{ backup_script.restore_file }}
+  if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.restore_command }}'  ; then
   {% else %}
-  if docker exec {{ backup_script.container }} {{ backup_script.restore_command }}  < $backup_file; then
+  if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.restore_command }}'  < $backup_file; then
   {% endif %}
 {% elif backup_script.directory is defined %}
-  if docker exec {{ backup_script.container }} sh -l -c 'cd {{backup_script.directory}}; rm -rf $(find . -depth 1); tar xzf -' < "$backup_file"; then
+  docker cp -L $backup_file {{ backup_script.container }}:/tmp/restore.tar.gz
+  if docker exec {{ backup_script.container }} sh -l -c 'cd {{backup_script.directory}}; rm -rf $(find . -maxdepth 1 | grep -v '^.$'); tar xzf /tmp/restore.tar.gz'; then
 {% endif %}
     message "END restore for {{ backup_script.name }}"
   else
