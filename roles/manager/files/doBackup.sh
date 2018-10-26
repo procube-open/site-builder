5f0dc7ca257ce6af26c9d8a6e456243c0b29d0e2
Mitsuru Nakakawaji <mitsuru@procube.jp>
2018-10-26 23:44:27 +0900

    support swarm and bactch script

--- roles/manager/files/doBackup.sh
+++ roles/manager/files/doBackup.sh
@@ -6,25 +6,46 @@ function message () {
   echo "INFO: $LOGGER:" $*
 }
 
-shopt -s expand_aliases
-source ~/.bashrc
-dlg-{{ inventory_hostname }}
 cd $BACKUPDIR
+export DOCKER_TLS_VERIFY=1
 
 {%  for backup_script in backup_scripts %}
+export DOCKER_HOST=tcp://{{ inventory_hostname }}:2376
+{% if backup_script.service is defined %}
+container_name=$(docker service ps {{ backup_script.service }}_{{ backup_script.container }} --format {% raw %}"{{.Name}}.{{.ID}}"{% endraw %} --filter desired-state=running --no-trunc | head -1)
+export DOCKER_HOST=$(docker service ps {{ backup_script.service }}_{{ backup_script.container }} --format {% raw %}"{{.Node}}"{% endraw %} --filter desired-state=running --no-trunc | head -1):2376
+if [ -z "$container_name" ]; then
+  message "NO Process for service: {{ backup_script.service }}_{{ backup_script.container }}, so skip it."
+else
+
+{% else %}
+container_name={{ backup_script.container }}
+{% endif %}
+message "ENTER CONTANER $container_name@$DOCKER_HOST"
+{% if backup_script.batch_scripts is defined %}
+  {% for batch_script in backup_script.batch_scripts %}
+message "START batch: {{ batch_script }}"
+if docker exec $container_name sh -l -c '{{ batch_script }}' 1>&2 ; then
+  message "END batch: {{ batch_script }}"
+else
+  message "FAIL batch: {{ batch_script }}"
+fi
+  {% endfor %}
+{% endif %}
+{% if backup_script.backup_command is defined or  backup_script.directory is defined %}
 # for backup {{ backup_script.name }}
 backup_file="backup-{{ backup_script.name }}-$(date +%Y%m%d%H%M%S).{{ backup_script.ext | default('tar.gz')}}"
 message "START backup {{ backup_script.name }} into $backup_file"
-{% if backup_script.backup_command is defined %}
-  {% if backup_script.backup_file is defined %}
-if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.backup_command }}' 1>&2 ; then
-  docker cp {{ backup_script.container }}:{{ backup_script.backup_file }} $backup_file
-  {% else %}
-if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.backup_command }}' > "$backup_file"; then
+  {% if backup_script.backup_command is defined %}
+    {% if backup_script.backup_file is defined %}
+if docker exec $container_name sh -l -c '{{ backup_script.backup_command }}' 1>&2 ; then
+  docker cp $container_name:{{ backup_script.backup_file }} $backup_file
+    {% else %}
+if docker exec $container_name sh -l -c '{{ backup_script.backup_command }}' > "$backup_file"; then
+    {% endif %}
+  {% elif backup_script.directory is defined %}
+if docker exec $container_name sh -l -c 'cd {{backup_script.directory}}; tar czf - $(find . -maxdepth 1 | grep -v '^.$')' > "$backup_file"; then
   {% endif %}
-{% elif backup_script.directory is defined %}
-if docker exec {{ backup_script.container }} sh -l -c 'cd {{backup_script.directory}}; tar czf - $(find . -maxdepth 1 | grep -v '^.$')' > "$backup_file"; then
-{% endif %}
   message "END backup for {{ backup_script.name }} from $backup_file"
   message "LINK backup-{{ backup_script.name }}-latest.{{ backup_script.ext | default('tar.gz')}} to $backup_file"
   rm -f "backup-{{ backup_script.name }}-latest.{{ backup_script.ext | default('tar.gz')}}"
@@ -32,5 +53,9 @@ if docker exec {{ backup_script.container }} sh -l -c 'cd {{backup_script.direct
 else
   message "FAIL backup for host {{ backup_script.name }}"
 fi
-
+{% endif %}
+message "LEAVE CONTANER $container_name@$DOCKER_HOST"
+{% if backup_script.service is defined %}
+fi
+{% endif %}
 {% endfor %}
--- roles/manager/files/doRestore.sh
+++ roles/manager/files/doRestore.sh
@@ -6,27 +6,37 @@ function message () {
   echo "INFO: $LOGGER:" $*
 }
 
-shopt -s expand_aliases
-source ~/.bashrc
-dlg-{{ inventory_hostname }}
 cd $BACKUPDIR
+export DOCKER_TLS_VERIFY=1
 
 {%  for backup_script in backup_scripts %}
+export DOCKER_HOST=tcp://{{ inventory_hostname }}:2376
+{% if backup_script.service is defined %}
+container_name=$(docker service ps {{ backup_script.service }}_{{ backup_script.container }} --format {% raw %}"{{.Name}}.{{.ID}}"{% endraw %} --filter desired-state=running --no-trunc | head -1)
+export DOCKER_HOST=$(docker service ps {{ backup_script.service }}_{{ backup_script.container }} --format {% raw %}"{{.Node}}"{% endraw %} --filter desired-state=running --no-trunc | head -1):2376
+if [ -z "$container_name" ]; then
+  message "NO Process for service: {{ backup_script.service }}_{{ backup_script.container }}, so skip it."
+else
+{% else %}
+container_name={{ backup_script.container }}
+{% endif %}
+message "ENTER CONTANER $container_name@$DOCKER_HOST"
+{% if backup_script.backup_command is defined or  backup_script.directory is defined %}
 # for restore {{ backup_script.name }}
 backup_file="backup-{{ backup_script.name }}-latest.{{ backup_script.ext | default('tar.gz') }}"
 if [ -r $backup_file ]; then
   message "START restore for {{ backup_script.name }} from $backup_file"
-{% if backup_script.restore_command is defined %}
-  {% if backup_script.restore_file is defined %}
-  docker cp -L $backup_file {{ backup_script.container }}:{{ backup_script.restore_file }}
-  if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.restore_command }}'  ; then
-  {% else %}
-  if docker exec {{ backup_script.container }} sh -l -c '{{ backup_script.restore_command }}'  < $backup_file; then
-  {% endif %}
-{% elif backup_script.directory is defined %}
+  {% if backup_script.restore_command is defined %}
+    {% if backup_script.restore_file is defined %}
+  docker cp -L $backup_file $container_name:{{ backup_script.restore_file }}
+  if docker exec $container_name sh -l -c '{{ backup_script.restore_command }}'  ; then
+    {% else %}
+  if docker exec $container_name sh -l -c '{{ backup_script.restore_command }}'  < $backup_file; then
+    {% endif %}
+  {% elif backup_script.directory is defined %}
   docker cp -L $backup_file {{ backup_script.container }}:/tmp/restore.tar.gz
-  if docker exec {{ backup_script.container }} sh -l -c 'cd {{backup_script.directory}}; rm -rf $(find . -maxdepth 1 | grep -v '^.$'); tar xzf /tmp/restore.tar.gz'; then
-{% endif %}
+  if docker exec $container_name sh -l -c 'cd {{backup_script.directory}}; rm -rf $(find . -maxdepth 1 | grep -v '^.$'); tar xzf /tmp/restore.tar.gz'; then
+  {% endif %}
     message "END restore for {{ backup_script.name }}"
   else
     message "FAIL restore for {{ backup_script.name }}"
@@ -34,4 +44,9 @@ if [ -r $backup_file ]; then
 else
   message "SKIP restore for {{ backup_script.name }} which does not have backup-{{ backup_script.name }}-latest"
 fi
+{% endif %}
+message "LEAVE CONTANER $container_name@$DOCKER_HOST"
+{% if backup_script.service is defined %}
+fi
+{% endif %}
 {% endfor %}
